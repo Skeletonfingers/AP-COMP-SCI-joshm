6972f4b5690240ff347a59c27f18d39a35ececed
Skeletonfingers <jmcnusa@gmail.com>
2016-11-14 09:25:44 -0800

    Add files via upload

---
+++ Project 7/src/Three.java
@@ -0,0 +1,21 @@
+
+public class Three
+{
+	public static void main(String[] args)
+	{
+		double[] x = {8, 4, 5, 21, 7, 9, 18, 2, 100};
+		System.out.println("Before rotation: ==============================");
+		for (int i = 0; i < x.length; i++)
+		{
+			System.out.println("x[" + i + "]: " + x[i]);
+		}
+		x = rotate(x, 3);
+		System.out.println("After rotation: ==============================");
+		for (int i = 0; i < x.length; i++)
+		{
+			System.out.println("x[" + i + "]: " + x[i]);
+		}
+ }
+
+ // Your method goes here.
+}
--- Project 7/src/Two.java
+++ Project 7/src/Two.java
@@ -8,15 +8,15 @@ public class Two
 		double[] x = new double[LENGTH];
 		
 		int currentSize = 0;
-		System.out.println("Enter up to 10 numbers, enter a letter when when you don't want to enter any more: ");
+		System.out.println("Enter up to 10 numbers, enter a letter when when you don't want to enter any more: "); //user enters 10 values and letter to stop entering
 		Scanner in = new Scanner(System.in);
-		while (currentSize < LENGTH && in.hasNextDouble())
+		while (currentSize < LENGTH && in.hasNextDouble()) //while size is < 10 and user is entering numbers loop
 		{
 			x[currentSize] = in.nextDouble();
 			currentSize++;		 
 		}
 		
-		for (int i = 0; i < x.length; i++)
+		for (int i = 0; i < x.length; i++) // put in separators
 		{
 			if (i > 0) {
 				System.out.print(" | ");
@@ -24,7 +24,17 @@ public class Two
 		 System.out.print(x[i]);
 		}
 		System.out.println();
-		
+		double total = 0;
+		for (int j = 0; j < currentSize; j++) //find total
+		{
+			total += x[j];
+		}
+		System.out.println("Total is: " + total);
+		findMin(x,currentSize);
+
+	}	
+	public static double findMin(double[] x, int currentSize) //find the minimum value
+	{
 		double small = x[0];
 		for (int k = 1; k < currentSize; k++) 
 		{
@@ -33,8 +43,9 @@ public class Two
 				small = x[k];
 			}
 		}
-		System.out.println("Minimum value:" + small);
-	}	
-
+		System.out.println("Minimum value: " + small);
+		return small;
+		
+	}
 }
 
